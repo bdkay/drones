4668b5aad4ff6410d7dbf137e4e3454b69a0fd00
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-25 17:00:21 -0500

    sega

--- src/app.js
+++ src/app.js
@@ -5,11 +5,11 @@ class Drone {
   get id(){
     return this._id + ' TEMP';
   }
-  set(value){
+  set id(value){
     this._id = value;
   }
 }
 
 let drone = new Drone('x3910');
-
+drone.id = 'b2111';
 console.log(`drone id: ${drone.id}`);
