ae7c4ac3524387ff8da7c950162774d819a512a6
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-25 17:11:42 -0500

    super

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
-class Drone {
-  constructor(id){
-    this._id = id;
-  }
-  get id(){
-    return this._id + ' TEMP';
-  }
-  set id(value){
-    this._id = value;
-  }
-}
+class Vehicle {
+  constructor(){
+    console.log('constructing vehicle');
+  };
+};
 
-let drone = new Drone('x3910');
-drone.id = 'b2111';
-console.log(`drone id: ${drone.id}`);
+class Drone extends Vehicle {
+
+};
+
+class Car extends Vehicle {
+  constructor(){
+    super();
+    console.log('constructing car');
+  };
+};
+
+let c = new Car();
