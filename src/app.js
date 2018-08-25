80740cab611546717ae2d30f7f1a0d177753b563
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-25 16:33:20 -0500

    class not instance

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ class Drone {
   }
 };
 
-let drone = new Drone('a123', 'tom');
+Drone.maxHeight = 2000;
 
-console.log(`drone: ${drone['id']} drone name: ${drone['name']}`);
+let drone1 = new Drone('a123', 'tom');
+let drone2 = new Drone('b456', 'steve');
+
+console.log(`${drone1.id} ${drone2.id} ${Drone.maxHeight}`);
