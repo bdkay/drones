1427333fbca81e580f699acac0992fd837888e43
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-25 17:26:48 -0500

    static method inheritance

--- src/app.js
+++ src/app.js
@@ -2,14 +2,22 @@ class Vehicle {
   start(){
     console.log('starting vehicle');
   }
+  static getCompanyName() {
+    console.log('my company');
+  }
 }
 
 class Car extends Vehicle {
   start(){
-    super.start();
     console.log('starting car');
+    super.start();
+  }
+  static getCompanyName() {
+    super.getCompanyName();
+    console.log('my other company');
   }
 }
 
 let c = new Car();
 c.start();
+Car.getCompanyName();
