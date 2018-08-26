ae49c639bed23187ad170524476ad9149cbcdbd4
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-25 21:30:36 -0500

    Data validation

--- src/FleetData.js
+++ src/FleetData.js
@@ -9,7 +9,7 @@ export let fleet = [
   },
   {
     license: 'XYZ456',
-    type: 'brone',
+    type: 'drone',
     model: 'Amazon 1550',
     airTimeHours: '2100',
     base: 'New York',
--- src/services/FleetDataService.js
+++ src/services/FleetDataService.js
@@ -14,8 +14,13 @@ export class FleetDataService {
     for (let data of fleet){
       switch(data.type){
         case 'car':
-          let car = this.loadCar(data);
-          this.cars.push(data);
+          if (this.validateCarData(data)){
+            let car = this.loadCar(data);
+            this.cars.push(data);
+          } else {
+            e = new DataError('Invalid car data', data);
+            this.errors.push(e);
+          }
           break;
         case 'drone':
           this.drones.push(data);
@@ -38,5 +43,21 @@ export class FleetDataService {
       this.errors.push(new DataError('error loading car', car));
     }
     return null;
+  }
+  validateCarData(car){
+    let requiredProps = 'license model latLong miles make'.split(' ');
+    let hasErrors = false;
+    
+    for (let field of requiredProps){
+      if (!car[field]){
+        this.errors.push(new DataError(`invalid field ${field}`, car));
+        hasErrors = true;
+      }
+    }
+    if (Number.isNaN(Number.parseFloat(car.miles))){
+      this.errors.push(new DataError('invalid mileage', car));
+      hasErrors = true;
+    }
+    return !hasErrors;
   }
 }
