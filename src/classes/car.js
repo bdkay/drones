f29b8d86af607e4c375a257fef0cccf22478b7d8
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-25 19:43:20 -0500

    More core classes ?

--- src/classes/car.js
+++ src/classes/car.js
@@ -1,5 +1,9 @@
 import { Vehicle } from './Vehicle.js';
 
 export class Car extends Vehicle {
-
+  constructor(make, miles){
+    super(license, model, latLong);
+    this.miles = null;
+    this.make = null;
+  }
 }
